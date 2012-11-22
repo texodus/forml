a93ea7804eafd2752db5dac1ae605f4e243d7d82
Andrew Stein <slink@slink.home>
2012-11-21 22:55:48 -0500

    Embedded prelude in the compiler - fixes #9

--- src/hs/Main.hs
+++ src/hs/Main.hs
@@ -36,7 +36,8 @@ import Forml.CLI
 
 import qualified Forml.Optimize as O
 
-import qualified Data.ByteString.Char8 as B
+--import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.UTF8 as B
 import           Data.FileEmbed
 
 
@@ -51,6 +52,7 @@ toHTML = toEntities . writeHtmlString defaultWriterOptions . readMarkdown defaul
 
 
 to_literate :: String -> String -> String
+to_literate "(Prelude)" = id
 to_literate filename
     | (head . tail . split "." $ filename) == "forml" = unlines . map l . lines
     | otherwise = id
@@ -65,18 +67,25 @@ to_parsed name src env = case parseForml name src of
                                            (as, []) -> Right (as, (x, src))
                                            (_, y)   -> Left y
 
+prelude' = B.toString $(embedFile "src/forml/prelude.forml")
+
+
 parse_forml :: [String] -> IO ([String], TypeSystem, [(Program, String)], [String])
-parse_forml xs = foldM parse' (xs, [], [], []) xs
+parse_forml xs = do xs' <- mapM get_source xs 
+                    foldM parse' ("prelude.forml" : xs, [], [], []) (prelude' : xs')
 
     where parse' :: ([String], TypeSystem, [(Program, String)], [String]) -> String -> IO ([String], TypeSystem, [(Program, String)], [String])
-          parse' (zs, ts, as, titles) filename =
+          parse' (zs, ts, as, titles) src'' =
           
-             do hFile <- openFile filename ReadMode
-                src'' <-   hGetContents hFile
-                let (title, src) = get_title src''
-                let src' = to_literate filename . (++ "\n") $ src
-                (ts', as') <- monitor [qq|Loading $filename|] $ return$ to_parsed (head zs) src' ts
+             do let (title, src) = get_title src''
+                let src' = to_literate (head zs) . (++ "\n") $ src
+                (ts', as') <- monitor [qq|Loading {head zs}|] $ return$ to_parsed (head zs) src' ts
                 return (tail zs, ts ++ ts', as ++ [as'], titles ++ [title])
+
+                
+          get_source filename =
+             do hFile <- openFile filename ReadMode
+                hGetContents hFile
 
 gen_js :: [String] -> [Program] -> (String, [(String, String)])
 gen_js src p = (compress (read' prelude ++ "\n" ++ (unlines $ map read' $ zipWith (render (Program $ get_program p)) src p)), [("",  read' prelude ++ "\n" ++ (unlines $ map read' $ zipWith (render_spec (Program $ get_program p)) src p))])
@@ -115,13 +124,14 @@ main' (parseArgs -> rc') =
           prettify = $(embedFile "lib/js/prettify.js") `mappend` $(embedFile "lib/js/lang-hs.js")
           
           css      = $(embedFile "lib/js/jasmine-1.0.1/jasmine.css") `mappend` $(embedFile "src/html/styles.css") `mappend` $(embedFile "lib/js/prettify.css")
-
           report   = $(embedFile "src/js/FormlReporter.js")
 
           htmljs   = "$('code').addClass('prettyprint lang-hs');prettyPrint();$('#run_tests').bind('click', prelude.html.table_of_contents)"
           console  = "prelude.html.console_runner()"
           
-          prelude  = $(embedFile "prelude.js");
+--          assump   = case S.decode $(embedFile "prelude.types") of
+--                       Left _ -> error "I Shouldn't be!"
+--                       Right x -> x
           
           watch' rc =
 
@@ -144,6 +154,8 @@ main' (parseArgs -> rc') =
               do (_, as, src', titles) <- parse_forml$ inputs rc
                  let src'' = O.run_optimizer (fmap fst src') as
                  let (js', ((_, tests'):_)) = gen_js (fmap snd src') src''
+                 
+               --  writeFile "prelude.types" (B.toString $ S.encode as)
 
                  js <- if optimize rc
                        then (monitor "Closure [libs"$ closure_local (js') "ADVANCED_OPTIMIZATIONS")
@@ -163,11 +175,11 @@ main' (parseArgs -> rc') =
                               yyy = map snd src'
                               html' xxx' yyy' = highlight (case xxx' of (Program xs) -> get_tests xs)$ toHTML (annotate_tests yyy' xxx')
                               html = concat $ zipWith html' xxx yyy
-                              prelude' = "<script>" ++ B.unpack jquery ++ B.unpack jasmine ++ B.unpack report ++ B.unpack prettify ++ js ++ tests ++ "</script>"
-                              css' = "<style type=\"text/css\">" ++ B.unpack css ++ "</style>"
+                              prelude' = "<script>" ++ B.toString jquery ++ B.toString jasmine ++ B.toString report ++ B.toString prettify ++ js ++ tests ++ "</script>"
+                              css' = "<style type=\"text/css\">" ++ B.toString css ++ "</style>"
                               hook = "<script>" ++ htmljs ++ ";window.document.title='"++(titles !! 0)++"';$('h1').html('" ++ (titles !! 0) ++ "')</script>"
 
-                          in do writeFile ((output rc) ++ ".html") (B.unpack header ++ css' ++ prelude' ++ html ++ hook ++ B.unpack footer)
+                          in do writeFile ((output rc) ++ ".html") (B.toString header ++ css' ++ prelude' ++ html ++ hook ++ B.toString footer)
                                 return $ Right ()
                      else return $ Right ()
 
@@ -179,7 +191,7 @@ main' (parseArgs -> rc') =
                              forkIO $ do errors <- hGetContents std_out
                                          putStr errors
                                          hFlush stdout
-                             hPutStrLn std_in$ B.unpack jasmine
+                             hPutStrLn std_in$ B.toString jasmine
                              hPutStrLn std_in$ js ++ "\n\n"
                              hPutStrLn std_in$ tests
                              hPutStrLn std_in$ console
@@ -194,7 +206,7 @@ main' (parseArgs -> rc') =
                      Phantom ->
                           monitor "Testing [Phantom.js]"$
                           do writeFile (output rc ++ ".phantom.js")
-                                   (B.unpack jquery ++ B.unpack jasmine ++ js ++ tests ++ console)
+                                   (B.toString jquery ++ B.toString jasmine ++ js ++ tests ++ console)
 
                              (Just std_in, Just std_out, _, p) <-
                                  createProcess (proc "phantomjs" [output rc ++ ".phantom.js"]) { std_in = CreatePipe, std_out = CreatePipe }
