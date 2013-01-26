3b43acf65b2ffe2116b69baf3bb3b0caaa98d2a5
Andrew Stein <slink@slink.home>
2013-01-26 18:27:25 -0500

    Removed demo test, attempt to fix travis CI sep conflict (stale cabal?)

--- src/hs/test/MainSpec.hs
+++ src/hs/test/MainSpec.hs
@@ -17,9 +17,13 @@ spec = do
         it "should compile the prelude.forml & the tests suite" $ do
         	main' test_config            
 
+src_files = [ "src/forml/prelude.forml"
+	        , "src/forml/tests.forml"
+	        , "src/forml/readme.forml" ]
+
 test_config =
 
-	RunConfig { inputs = ["src/forml/prelude.forml", "src/forml/tests.forml"]
+	RunConfig { inputs = src_files
               , output = "default.js"
               , show_types = True
               , optimize   = True
