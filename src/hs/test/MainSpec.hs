a63cd68e0d6a4748a5d449c90c0c233f0ba31330
Andrew Stein <slink@slink.home>
2013-01-26 22:04:15 -0500

    Verified compiler output

--- src/hs/test/MainSpec.hs
+++ src/hs/test/MainSpec.hs
@@ -1,6 +1,11 @@
 
 module MainSpec where
 
+import System.IO.Silently
+
+import Data.ByteString (pack)
+import Data.Char (ord)
+
 import Test.Hspec
 import Test.Hspec.HUnit
 import Test.Hspec.QuickCheck (prop)
@@ -15,21 +20,28 @@ spec = do
 
     describe "Forml Compiler" $ do
         it "should compile the prelude.forml & the tests suite" $ do
-        	main' test_config            
+        	(std_out, _) <- capture $ main' test_config
+        	if length std_out > 1535 && length std_out < 1545
+        	    then return ()
+        	    else assertFailure $ (show $ length std_out) ++ "\n" ++ std_out 
 
+src_files :: [[Char]]
 src_files = [ "src/forml/prelude.forml"
 	        , "src/forml/tests.forml"
 	        , "src/forml/readme.forml" ]
 
+test_config :: RunConfig
 test_config =
 
 	RunConfig { inputs = src_files
               , output = "default.js"
-              , show_types = True
+              , show_types = False
               , optimize   = True
               , silent     = False
-              , flush      = True
-              , run_tests  = NoTest
+              , flush      = False
+              , run_tests  = Phantom
               , write_docs = True
               , implicit_prelude = False
               , watch      = False }
+
+
